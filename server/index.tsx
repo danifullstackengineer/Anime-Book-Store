84ccef31621f3b619935b4ba7521d091cd54c0fb
danifullstack <danifullstackengineer@gmail.com>
2022-03-13 00:53:28 +0100

    finished structure

--- server/index.tsx
+++ server/index.tsx
@@ -1,16 +1,12 @@
 import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
-import bodyParser from "body-parser";
-import path from "path";
-import { fileURLToPath } from "url";
-import dotenv from "dotenv";
-import router from "./router";
-import Stripe from "stripe";
-dotenv.config();
+import cors from 'cors';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import path from 'path';
+require('dotenv').config();
+import router from './router';
+import Stripe from 'stripe';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const app = express();
 
 app.use(bodyParser.json());
@@ -22,7 +18,7 @@ app.use("/", router);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
-  app.get("*", (req, res) => {
+  app.get("*", (req:any, res:any) => {
     res.sendFile(
       path.resolve(__dirname, "..", "client", "build", "index.html")
     );
@@ -36,4 +32,4 @@ mongoose
     console.log("Connected to database...");
     app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
   })
-  .catch((err) => console.log(err));
+  .catch((err:any) => console.log(err));
